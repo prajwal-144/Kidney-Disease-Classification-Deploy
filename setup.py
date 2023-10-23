417cdea2beb9ff997ab1ddd3eff682b6701ee427
Prajwal Uday <prajwal.uday@learner.manipal.edu>
2023-10-23 12:20:45 +0530

    Add new files

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_requirements(file_path:str)->List[str]:
     return requirements
 
 setup(
-name='FOREX-Deployment',
+name='Kidney-Disease-Classification-Deploy',
 version='0.0.1',
 author='Prajwal',
 author_email='prajwal.uday@learner.manipal.edu',
