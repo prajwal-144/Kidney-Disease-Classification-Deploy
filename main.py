01d45c46329e0ee5c021866c56e1992333183133
Prajwal Uday <prajwal.uday@learner.manipal.edu>
2023-10-23 19:21:06 +0530

    Model Trainer Initialized

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -20,6 +22,18 @@ try:
     obj = PrepareBaseModelTrainingPipeline()
     obj.main()
     logger.info(f"------ {STAGE_NAME} completed ------\n\n==========")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f"------ {STAGE_NAME} started ------")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f"------ stage {STAGE_NAME} completed ------\n\n==========")
 except Exception as e:
     logger.exception(e)
     raise e
